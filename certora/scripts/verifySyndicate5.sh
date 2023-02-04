65de990b13bb278c86acd4e01cff255afb2127e7
teryanarmen <61996358+teryanarmen@users.noreply.github.com>
2023-02-03 21:12:36 -0800

    Create verifySyndicate5.sh

---
+++ certora/scripts/verifySyndicate5.sh
@@ -0,0 +1,14 @@
+certoraRun  certora/harnesses/SyndicateHarness.sol \
+    certora/harnesses/MockStakeHouseUniverse.sol \
+    certora/harnesses/MockStakeHouseRegistry.sol \
+    certora/harnesses/MockSlotSettlementRegistry.sol \
+    certora/harnesses/MocksETH.sol \
+    --verify SyndicateHarness:certora/specs/Syndicate.spec \
+    --cloud master \
+    --optimistic_loop \
+    --optimize 1 \
+    --loop_iter 3 \
+    --rules lastAccumulatedETHPerFreeFloatingShareCannotChangeOnceDeregistered sETHUsETHUserClaimForKnot_sound completeInvariant imprecisionIsBounded impressisionIsBounded \
+    --rule_sanity \
+    --settings -optimisticFallback=true \
+    --packages @blockswaplab=node_modules/@blockswaplab @openzeppelin=node_modules/@openzeppelin \
