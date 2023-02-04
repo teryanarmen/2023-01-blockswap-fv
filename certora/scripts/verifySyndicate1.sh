f6016a496ecb6ea6e7955eb577aac56eeb2875ea
teryanarmen <61996358+teryanarmen@users.noreply.github.com>
2023-02-03 21:04:14 -0800

    Update and rename verifySyndicate.sh to verifySyndicate1.sh

--- certora/scripts/verifySyndicate1.sh
+++ certora/scripts/verifySyndicate1.sh
@@ -1,13 +1,4 @@
-# Call this script from root, e.g. `sh certora/scripts/verifySyndicate.sh $1 $2`
-# where $1 is a comment for the run that gives some background to the call and $2 is an 
-# optional arguement used to specify a rule, without a specified rule all rules in the 
-# spec will be ran.
-if [[ "$2" ]]
-then
-    RULE="--rule $2"
-fi
 
-solc-select use 0.8.13
 
 certoraRun  certora/harnesses/SyndicateHarness.sol \
     certora/harnesses/MockStakeHouseUniverse.sol \
@@ -19,8 +10,10 @@ certoraRun  certora/harnesses/SyndicateHarness.sol \
     --optimistic_loop \
     --optimize 1 \
     --loop_iter 3 \
-    $RULE \
+    --rules canNotDegisterUnregisteredKnot_ totalEthReceivedMonotonicallyIncreases noOneCanAlterTheMoneyYouHave noOneCanAlterTheMoneyYouHave_2 stakeThenUnstake \
     --rule_sanity \
     --settings -optimisticFallback=true \
     --packages @blockswaplab=node_modules/@blockswaplab @openzeppelin=node_modules/@openzeppelin \
     --msg "Syndicate $1 $2"
+    
+    
